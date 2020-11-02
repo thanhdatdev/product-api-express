7045a18734743bda61a2410ee376f55373a21a64
FIRST_NAME LAST_NAME <thanhdat.nguyen2104@gmail.com>
2020-11-02 11:41:55 +0700

    'add-access-key'

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({
     region: "ap-southeast-1",
-    endpoint: "http://localhost:8000",
-    accessKeyId: '',
-    secretAccessKey: ''
+    endpoint: "ec2-18-139-3-196.ap-southeast-1.compute.amazonaws.com",
+    accessKeyId: 'AKIAJ2777G2KVWOJBWDQ',
+    secretAccessKey: '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'
 });
