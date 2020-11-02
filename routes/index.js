490c45860bfeae6c435edc6f95a34d5acb86e76d
FIRST_NAME LAST_NAME <thanhdat.nguyen2104@gmail.com>
2020-11-02 19:19:20 +0700

    'refactor-coding'

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,12 @@ router.post('/students/:maSinhVien/:tenSinhVien', ((req, res) => {
 }));
 
 router.post('/students/update/:maSinhVien/:tenSinhVien/:ngaySinh/:avatar', ((req, res, next) => {
-    res.render('updateForm', { maSinhVien: req.params.maSinhVien, tenSinhVien: req.params.tenSinhVien, ngaySinh: req.params.ngaySinh, avatar: req.params.avatar });
+    res.render('updateForm', {
+        maSinhVien: req.params.maSinhVien,
+        tenSinhVien: req.params.tenSinhVien,
+        ngaySinh: req.params.ngaySinh,
+        avatar: req.params.avatar
+    });
 }));
 
 router.post('/students/update/:maSinhVien/:tenSinhVien', ((req, res) => {
