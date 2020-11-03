5a571de4e96672c952173277e36ee5fe01935334
ThanhDatNguyen <thanhdat.nguyen2104@gmail.com>
2020-11-03 13:07:49 +0700

    'refactor-coding'

--- config/database.js
+++ config/database.js
@@ -2,9 +2,7 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({
     region: "ap-southeast-1",
-
-    //endpoint: "http://dynamodb.ap-southeast-1.amazonaws.com",
-    endpoint: "http://localhost:8000",
+    endpoint: "http://dynamodb.ap-southeast-1.amazonaws.com",
     accessKeyId: 'AKIAJ4O2NKO4SSYMYVHA',
     secretAccessKey: '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'
 });
